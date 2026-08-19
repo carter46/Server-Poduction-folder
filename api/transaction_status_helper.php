0f7ec53506b08ab71867c66b14b7e8b38e7563b2
Signature Solutions <sigsol2024@gmail.com>
2026-08-19 15:24:58 +0100

    g

--- api/transaction_status_helper.php
+++ api/transaction_status_helper.php
@@ -34,6 +34,7 @@ function applyTransactionStatusBalanceDelta(PDO $pdo, $accountTable, $amount, $o
         'zenith_bank_account_settings',
         'access_bank_account_settings',
         'wema_bank_account_settings',
+        'polaris_bank_account_settings',
     ];
     if (!in_array($accountTable, $allowedTables, true)) {
         throw new Exception('Invalid account table');
